5d7d0e8ed9d10ddaad03fed7b75fd56074ddd726
Cui Liqiang <cui.liqiang@gmail.com>
2013-04-08 07:19:25 +0800

    works with one concrete class dependency

--- src/main/java/domain/AppRunner.java
+++ src/main/java/domain/AppRunner.java
@@ -1,10 +1,17 @@
 package domain;
 
 import core.annotation.Component;
+import javax.inject.Inject;
 
 @Component
 public class AppRunner {
+    private Car car;
+
+    @Inject public AppRunner(Car car) {
+        this.car = car;
+    }
+
     public void run() {
-        System.out.println("works");
+        car.run();
     }
 }
---
+++ src/main/java/domain/Car.java
@@ -0,0 +1,10 @@
+package domain;
+
+import core.annotation.Component;
+
+@Component
+public class Car {
+    public void run() {
+        System.out.println("car is running");
+    }
+}
